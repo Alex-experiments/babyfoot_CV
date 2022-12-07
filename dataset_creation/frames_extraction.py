af1f1f1564ba30f55416ddfed2b71562e68c035a
alex.gitanerie <57791386+Alex-experiments@users.noreply.github.com>
2022-12-07 12:22:57 +0100

    unique images extraction

--- dataset_creation/frames_extraction.py
+++ dataset_creation/frames_extraction.py
@@ -11,21 +11,20 @@ DEFAULT_NB_FRAMES = 100
 def extract_frames_single_video(input_file_path: str, output_folder_path: str, nb_frames: int) -> None:
     _, tail = os.path.split(input_file_path)
     file_name = tail.split(".")[0]
-    count = 0
+    used_frames = []
     for file in os.listdir(output_folder_path):
         if file.startswith(file_name) and file.endswith(".jpg"):
-            count += 1
-    print(count)
+            used_frames.append(int(file.split('_')[-1][:-4])) #le :-4 c'estpour virer le .jpg
     
     vidcap = cv2.VideoCapture(input_file_path)
     video_length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    kept_frames = set(random.sample(range(video_length), k=nb_frames))
+    available_frames = set(range(video_length)) - set(used_frames)
+    kept_frames = random.sample(available_frames, k=nb_frames)
 
-    for frame in tqdm.tqdm(range(max(kept_frames) + 1)):
+    for count, frame in enumerate(tqdm.tqdm(range(max(kept_frames) + 1))):
         _, image = vidcap.read()
         if frame in kept_frames: 
             cv2.imwrite(os.path.join(output_folder_path, f"{file_name}_{count}.jpg"), image) # save frame as JPEG file
-            count += 1
 
 def extract_frames_all_videos(
     input_folder_path: str = DEFAULT_INPUT_FOLDER_PATH,
